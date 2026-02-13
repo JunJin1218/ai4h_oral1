58600195e63c6c2b71c6f32cb95d2e4705d1cb99
Sofeanna.Y <1007016@mymail.sutd.edu.sg>
2026-02-13 18:46:35 +0800

    embedding

--- image_preprocess.py
+++ image_preprocess.py
@@ -12,25 +12,47 @@ from image_embedding.vit import get_image_embedding, load_vit_model
 
 
 def main() -> None:
-    input_dir = Path("input_img")
-    output_dir = Path("data")
+    input_dir = Path(
+        r"C:\Users\Asus\T5 - SDS\T8-AI4H\Oral Dose Forms\Individual tablets capsules"
+    )
+    output_dir = Path(
+        r"C:\Users\Asus\T5 - SDS\T8-AI4H\Individual Capsules"
+    )
     output_dir.mkdir(parents=True, exist_ok=True)
 
     processor, model, device = load_vit_model()
 
     image_paths = sorted(
-        p for p in input_dir.iterdir() if p.suffix.lower() in {".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"}
+        p for p in input_dir.rglob("*")
+        if p.suffix.lower() in {".gif",".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"}
     )
 
     if not image_paths:
         print(f"No images found in {input_dir.resolve()}")
         return
+    
+    failed = []
 
     for img_path in image_paths:
-        embedding = get_image_embedding(model, processor, img_path, device=device, pooling="cls")
-        out_path = output_dir / f"{img_path.stem}.pt"
-        torch.save(embedding, out_path)
-        print(f"Saved: {out_path}")
+        try:
+            embedding = get_image_embedding(
+                model, processor, img_path, device=device, pooling="cls"
+            )
+            out_path = output_dir / f"{img_path.stem}.pt"
+            torch.save(embedding, out_path)
+        except Exception as e:
+            failed.append((img_path, str(e)))
+
+    print(f"Finished. Failed images: {len(failed)}")
+    for p, err in failed:
+        print("FAILED:", p, err)
+
+
+    # for img_path in image_paths:
+    #     embedding = get_image_embedding(model, processor, img_path, device=device, pooling="cls")
+    #     out_path = output_dir / f"{img_path.stem}.pt"
+    #     torch.save(embedding, out_path)
+    #     print(f"Saved: {out_path}")
 
 if __name__ == "__main__":
     main()
